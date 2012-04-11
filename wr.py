f1820472e51f8c2b381f7e0c595c0c3405797364
waawal <waawal@boom.ws>
2012-04-11 20:57:40 +0400

    now handles sequences of tuples and dicts

--- wr.py
+++ wr.py
@@ -1,5 +1,6 @@
 from itertools import islice
 from random import randint
+from collections import Mapping
 
 def wr(data):
     """ Returns a key of the passed in dict.
@@ -10,10 +11,13 @@ def wr(data):
     def build_population(data):
         """ Builds a sample population of the data.
         """
-        for item, weight in data.items():
+        if isinstance(data, Mapping):
+            dataitems = data.items()
+        else:
+            dataitems = data
+        for item, weight in dataitems:
             for _ in range(weight):
                 yield item
-
     population = build_population(data)
     populationsize = sum(data.values())
     index = randint(0, populationsize - 1)
