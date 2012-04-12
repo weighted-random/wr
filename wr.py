2a7c2dcdbd4f450eb6d8ee9182d66ccdc28da720
waawal <waawal@boom.ws>
2012-04-13 00:32:58 +0400

    90\% Speed increase! New main implementation based on inplaceunsorted

--- wr.py
+++ wr.py
@@ -1,30 +1,18 @@
-from itertools import islice
-from random import randint
+import random
 from collections import Mapping
 
 def choice(data):
-    """ Returns a key of a passed in mapping containing weights
-        in it's corresponding values. It can also work on a
-        sequence of pairs consisting of (item, weight).
-    """
-
-    def build_population(data):
-        """ Builds a sample population of the data.
-        """
-        if isinstance(data, Mapping):
-            dataitems = data.items()
-        else:
-            dataitems = data
-        for item, weight in dataitems:
-            for _ in range(weight):
-                yield item
-
-    population = build_population(data)
     if isinstance(data, Mapping):
-        populationsize = sum(data.values())
+        dataitems = data.items()
+        totalweights = sum(data.values()) - 1
     else:
-        populationsize = sum([i[1] for i in data])
-    index = randint(0, populationsize - 1)
-    return islice(population, index, None).next()
-
+        dataitems = data
+        totalweights = sum([i[1] for i in data]) - 1
+    
+    randomindex = random.randint(0, totalweights)
+    count = 0
+    for returnable, weight in dataitems:
+            count += weight
+            if count > randomindex:
+                    return returnable
 
